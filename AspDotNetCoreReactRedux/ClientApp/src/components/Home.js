cde577978c68323906e397b39a9fb1c4c2d3359f
walif <walif.corp@gmail.com>
2020-11-27 21:00:59 +0600

    Some update data table

--- AspDotNetCoreReactRedux/ClientApp/src/components/Home.js
+++ AspDotNetCoreReactRedux/ClientApp/src/components/Home.js
@@ -9,49 +9,87 @@ const columns = [
         cell: (row, index) => <div>{index + 1}</div>,
         width: "70px"
     }, {
-        name: 'Post',
-        selector: 'postTitle',
+        name: 'Title',
+        selector: 'title',
         sortable: true
     },
     {
-        name: 'postCreateBy',
-        selector: 'postCreateBy',
+        name: 'Create By',
+        selector: 'createBy',
     },
     {
-        name: 'postCreateDateTime',
-        selector: 'postCreateDateTime',
+        name: 'Create Date',
+        selector: 'createDate',
     },
     {
-        name: 'comment',
-        selector: 'comment',
-    },
-    {
-        name: 'commentCreateBy',
-        selector: 'commentCreateBy',
-    },
-    {
-        name: 'commentCreateDateTime',
-        selector: 'commentCreateDateTime',
-    },
-    {
-        name: 'likeVote',
-        selector: 'likeVote',
-    },
-    {
-        name: 'dislikeVote',
-        selector: 'dislikeVote',
+        name: '',
+        selector: 'count',
     }
 ];
 
 class Home extends Component {
 
+    state = {
+        result:[]
+    }
+
     componentDidMount() {
         //console.log();
         this.props.loadPostComment()
     }
+
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        if (nextProps.postComnt) {
+            if (nextProps.postComnt.result.length != this.state.result.length) {
+                this.setState({
+                    result: this.GenerateResult(nextProps.postComnt.result)
+                });
+            }
+        }
+    }
+
+    GenerateResult = (result) => {
+        var data = [];
+        var newRecord = [];
+        //let x = result.map(item => [item.postId, item.postTitle, item.postCreateBy, item.postCreateDateTime, item.countComment]);
+        //let unique = result.filter((x, i, a) => a.indexOf(x.postId) == i);
+        //let unique = [...new Set(x.map(item => item))]; 
+        const map = new Map();
+        for (const item of result) {
+            if (!map.has(item.postId)) {
+                map.set(item.postId, true);    // set any value to Map
+                data.push({
+                    postId: item.postId,
+                    postTitle: item.postTitle,
+                    postCreateBy: item.postCreateBy,
+                    postCreateDateTime: item.postCreateDateTime,
+                    countComment: item.countComment,
+                });
+            }
+        }
+        for (const item of data) {
+            newRecord.push({
+                title: item.postTitle,
+                createBy: item.postCreateBy,
+                createDate: item.postCreateDateTime,
+                count: item.countComment + ' Comments'
+            });
+            let comments = result.filter(c => c.postId === item.postId);
+            for (const com of comments) {
+                newRecord.push({
+                    title: com.comment,
+                    createBy: com.commentCreateBy,
+                    createDate: com.commentCreateDateTime,
+                    count: com.likeVote + ' || ' + com.dislikeVote
+                });
+            }
+        }
+        //console.log(data);
+        return newRecord;
+    }
     
     render() {
-        const { result } = this.props.postComnt
+        const { result } = this.state
         //console.log(this.props);
         return (
             <div>
